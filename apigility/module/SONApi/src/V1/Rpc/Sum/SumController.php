aacb686d033b01db9d449bb0a952ec3d66090be2
Onesimo Batista <onesimobatista@gmail.com>
2016-11-18 23:29:58 -0200

    created sum RPC function

--- apigility/module/SONApi/src/V1/Rpc/Sum/SumController.php
+++ apigility/module/SONApi/src/V1/Rpc/Sum/SumController.php
@@ -2,10 +2,17 @@
 namespace SONApi\V1\Rpc\Sum;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use ZF\ContentNegotiation\ViewModel;
 
 class SumController extends AbstractActionController
 {
     public function sumAction()
     {
+        $num1 = $this->params()->fromQuery('num1');
+        $num2 = $this->params()->fromQuery('num2');
+        $sum = $num1 + $num2;
+        return new ViewModel([
+           'sum' => $sum 
+        ]);
     }
 }
