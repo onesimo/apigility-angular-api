2fd1a64409ba2596f5d3ef6970ad14c7a98b34b2
Onesimo Batista <onesimobatista@gmail.com>
2016-11-18 23:20:37 -0200

    create sumController RPC

--- apigility/module/SONApi/config/module.config.php
+++ apigility/module/SONApi/config/module.config.php
@@ -1,3 +1,54 @@
 <?php
- return [
+return [
+    'controllers' => [
+        'factories' => [
+            'SONApi\\V1\\Rpc\\Sum\\Controller' => \SONApi\V1\Rpc\Sum\SumControllerFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'son-api.rpc.sum' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route' => '/sum',
+                    'defaults' => [
+                        'controller' => 'SONApi\\V1\\Rpc\\Sum\\Controller',
+                        'action' => 'sum',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'zf-versioning' => [
+        'uri' => [
+            0 => 'son-api.rpc.sum',
+        ],
+    ],
+    'zf-rpc' => [
+        'SONApi\\V1\\Rpc\\Sum\\Controller' => [
+            'service_name' => 'Sum',
+            'http_methods' => [
+                0 => 'GET',
+            ],
+            'route_name' => 'son-api.rpc.sum',
+        ],
+    ],
+    'zf-content-negotiation' => [
+        'controllers' => [
+            'SONApi\\V1\\Rpc\\Sum\\Controller' => 'Json',
+        ],
+        'accept_whitelist' => [
+            'SONApi\\V1\\Rpc\\Sum\\Controller' => [
+                0 => 'application/vnd.son-api.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ],
+        ],
+        'content_type_whitelist' => [
+            'SONApi\\V1\\Rpc\\Sum\\Controller' => [
+                0 => 'application/vnd.son-api.v1+json',
+                1 => 'application/json',
+            ],
+        ],
+    ],
 ];
---
+++ apigility/module/SONApi/src/V1/Rpc/Sum/SumController.php
@@ -0,0 +1,11 @@
+<?php
+namespace SONApi\V1\Rpc\Sum;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class SumController extends AbstractActionController
+{
+    public function sumAction()
+    {
+    }
+}
